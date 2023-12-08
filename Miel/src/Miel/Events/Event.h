a5211b9f2fd6b5954cafec5f4659f35297e345cc
therealazzy <arslanboulafrad@gmail.com>
2023-12-08 20:34:28 +0000

    code cleanup

--- Miel/src/Miel/Events/Event.h
+++ Miel/src/Miel/Events/Event.h
@@ -21,7 +21,7 @@ namespace Miel {
 
 	enum EventCategory
 	{
-		None = 2,
+		None = 0,
 		EventCategoryApplication        = BIT(0),
 		EventCategoryInput				= BIT(1),
 		EventCategoryKeyboard			= BIT(2),
